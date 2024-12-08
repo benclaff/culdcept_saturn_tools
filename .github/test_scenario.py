0408d020db8c8d44e8f326b240a6f4c0b16485b2
benclaff <benclaff31@gmail.com>
2024-12-08 21:04:04 +0100

    ongoing work on test_scenario.py

--- .github/test_scenario.py
+++ .github/test_scenario.py
@@ -1,2 +1,29 @@
-print("Testing scenrarrio !")
+import yaml
+from yaml.loader import SafeLoader
+
+print("Testing scenario translation integrity")
+data = dict()
+with open("translations/scenario.yaml") as stream:
+    try:
+        data = yaml.load(stream, Loader=SafeLoader)
+        print("Blocks: " + str(len(data.keys())))
+
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit(1)
+
+for block, val in data.items():
+    print(block)
+    print(val['offsets'])
+    byte_length = val['offsets']['byte_length']
+    print(byte_length)
+    print(val['text']['original'])
+    print(val['text']['translated'])
+    bytes_original = str(val['text']['original']).encode('shift_jisx0213')
+    bytes_original = bytes_original.replace('#0A#'.encode('shift_jisx0213'), b'\x0A')
+    print("to "+str(bytes_original))
+    print(len(bytes_original))
+
+
+
 exit(0)
