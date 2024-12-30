ee341351bee74b7499d0b3665d0ddc2900c78b8e
benclaff <benclaff31@gmail.com>
2024-12-30 14:05:49 +0100

    helpscript almost functionnal, seems to lack some text

--- src/exraction_help_script.py
+++ src/exraction_help_script.py
@@ -36,6 +36,8 @@
 # There are 2 bytes of unknown function at bytes at C12AA4 ==> text? (need to be confirmed in-game)
 
 import re
+from typing import Dict
+
 import meta
 import yaml
 
@@ -49,5 +51,145 @@ dialog_sequence_pattern = re.compile(
 
 # look for this pointer pattern
 pointer_pattern = re.compile(
-    b'(?P<pointer>(\\x10[\\xF0-\\xF9]\\x02)[\\x00]+(?P<index>[\\x00-\\xFF])(?P<offset>([\\x00-\\FF][\\x00-\\xFF])))'
+    b'(?P<pointer>(\\x10[\\xF0-\\xF9]\\x02)([\\x00]{1,6})(?P<index>[\\x00-\\xFF]{1})(?P<offset>([\\x00-\\xFF]{2})))'
 )
+
+def pointer_offsets_extraction(block_start_offset: int, data) -> Dict:
+    d = dict()
+    for i, m in enumerate(re.finditer(pointer_pattern, data)):
+        match_dic = m.groupdict()
+        #negative offset
+        if match_dic["offset"] > b'\\x0F\\xFF':
+            new_block_start_offset = int(match_dic["offset"].hex(),16) - int('FFFF',16) - 1
+        #positive offset
+        else:
+            new_block_start_offset = int.from_bytes(match_dic["offset"])
+        new_offset = block_start_offset + m.end() - 2 + new_block_start_offset
+        if new_offset < 0 :
+            print("offest computation error")
+            exit(1)
+        d[new_offset] = hex(new_offset).lstrip("0x")
+    return d
+
+# open saturn file
+with open(meta.DT0, 'rb') as f:
+    data = f.read()
+
+counter = 0
+output_yaml = ""
+
+table_bytes= data[int(_TABLE_START, 16):(int(_TABLE_END, 16) + 1)]
+
+#get all offsets from table
+table_offsets = dict()
+for i in range(0, len(table_bytes)):
+    if i%2==0:
+        table_offsets[int(table_bytes[i:i+2].hex(),16)] = table_bytes[i:i+2].hex()
+table_offsets = dict(sorted(table_offsets.items()))
+
+#get more offsets from pointers
+
+# get text blocks per offset, convert them to a yaml
+prev_offset=0
+prev_offset_str="0000"
+s = int(_TABLE_START,16)
+yaml_str: str = ""
+new_offsets = dict()
+for i,(offset_int,offset_str) in enumerate(table_offsets.items()):
+    print("########" + str(i))
+    if i>0:
+        print("table relative:\t"+prev_offset_str+":"+offset_str)
+        start = s + prev_offset
+        end = s + offset_int
+        print("file relative:\t"+hex(start)+":"+hex(end))
+        block=data[start:end]
+        new_offsets[start] = hex(start).lstrip("x0")
+        #extract pointers from each block
+        more_offsets = pointer_offsets_extraction(start,block)
+        new_offsets.update(more_offsets)
+    prev_offset=offset_int
+    prev_offset_str=offset_str
+
+print("# text blocks found: "+str(len(table_offsets.keys())))
+print("# text blocks after pointer analysis: "+str(len(new_offsets.keys())))
+
+
+def bytes_to_yaml_text(text_block) -> str:
+    yaml_str = ""
+    for i,m in enumerate(re.finditer(dialog_sequence_pattern, text_block)):
+        match_dic = m.groupdict()
+        yaml_str += "\n    sequence_" + str(i) + ":"
+        yaml_str += "\n      start_relative_to_block: " + str(m.start())
+        yaml_str += "\n      end_relative_to_block: " + str(m.end())
+        # if match_dic["head"] is not None:
+        #     yaml_str += "\n      head_hexa: " + match_dic["head"].hex()
+        # if match_dic["tail"] is not None:
+        #     yaml_str += "\n      tail_hexa: " + match_dic["tail"].hex()
+        yaml_str += "\n      portrait: " + match_dic["portrait"].hex()
+        dec = match_dic["text"]
+        dec = dec.replace(b'\x13\x07', "\\p".encode('shift_jisx0213'))
+        dec = dec.replace(b'\x0A', '\\n'.encode('shift_jisx0213'))
+        dec = dec.replace(b'\x07', '\\w'.encode('shift_jisx0213'))
+        # value = value.replace(b'\x00', '[00]'.encode('shift_jisx0213'))
+        try:
+            dec = dec.decode('shift_jisx0213', errors='strict')
+            yaml_str += "\n      original_txt: " + dec
+            yaml_str += "\n      ruler_helper: " + "-----------------------|-----------------------|-----------------------|"
+            yaml_str += "\n      translat_txt: "
+        except UnicodeError as ex:
+            print(ex.with_traceback(ex))
+            exit(1)
+    return yaml_str
+
+# output yaml using sorted offsets
+sorted_offsets = dict(sorted(new_offsets.items()))
+prev_offset=0
+prev_offset_str="0000"
+yaml_str: str = ""
+i=0
+for offset_int,offset_str in sorted_offsets.items():
+    print("--------" + str(i))
+    if i>0:
+        start = prev_offset
+        end = offset_int
+        print("file relative:\t"+hex(start)+":"+hex(end))
+        text_block=data[start:end]
+        offset_yaml_str = "\nhelpscript_block_"+str(i)+":"
+        offset_yaml_str += "\n  offsets:"
+        offset_yaml_str += "\n    byte_length: "+str(len(text_block))
+        offset_yaml_str += "\n    start: "+hex(start)
+        offset_yaml_str += "\n    end: "+hex(end)
+        offset_yaml_str += "\n  sequences: "
+        print("data:\t\t\t"+text_block.hex())
+        offset_yaml_str += bytes_to_yaml_text(text_block)
+        print(offset_yaml_str)
+        # write yaml output
+        data_yaml = yaml.safe_load(offset_yaml_str)
+        with open('translations/helpscript/helpscript_block'+str(i)+'.yaml', 'w') as file:
+            yaml.dump(data_yaml, file, default_flow_style=False, allow_unicode=True, sort_keys=False)
+        yaml_str += offset_yaml_str
+    i+=1
+    prev_offset=offset_int
+    prev_offset_str=offset_str
+
+#last block
+_SCENARIO_END = "C11C4D"
+
+start = prev_offset
+end = int(_SCENARIO_END,16)
+print("file relative:\t"+hex(start)+":"+hex(end))
+text_block=data[start:end]
+offset_yaml_str = "\nhelpscript_block_"+str(i)+":"
+offset_yaml_str += "\n  offsets:"
+offset_yaml_str += "\n    byte_length: "+str(len(text_block))
+offset_yaml_str += "\n    start: "+hex(start)
+offset_yaml_str += "\n    end: "+hex(end)
+offset_yaml_str += "\n  sequences: "
+print("data:\t\t\t"+text_block.hex())
+offset_yaml_str += bytes_to_yaml_text(text_block)
+print(offset_yaml_str)
+# write yaml output
+data_yaml = yaml.safe_load(offset_yaml_str)
+with open('translations/helpscript/helpscript_block'+str(i)+'.yaml', 'w') as file:
+    yaml.dump(data_yaml, file, default_flow_style=False, allow_unicode=True, sort_keys=False)
+yaml_str += offset_yaml_str
