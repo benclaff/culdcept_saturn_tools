339f3c81f2d12a82f87af6d1620157ac5ce385d6
benjamin linard (HP portable) <benjamin.linard@inrae.fr>
2025-05-22 22:30:51 +0200

    notes on cards

--- src/extraction_card_data.py
+++ src/extraction_card_data.py
@@ -15,11 +15,12 @@
 # Card 103   00 00 00 00 00 32 00 32 00 5A 01 03 28 00 1B 22 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
 # Card 123   00 00 00 00 00 00 00 3C 00 46 00 0D 1A 00 10 53 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 12 00 00 00 00 00 00 00 00 00 00
 # Card 126   00 00 00 00 00 3C 00 50 00 B4 02 15 A9 00 10 53 00 00 00 02 00 01 00 00 00 00 00 00 00 00 00 00 00 41 00 80 00 43 00 88 00 14
-#                                                                         |cost: 風
-#                                                                      |cost: 森
-#                                                                   |cost: 水
-#                                                                |cost: 火
-# Zoom on XX columns, those are boolean for card status
+#                                                                         |cost: 風 (wind)
+#                                                                      |cost: 森 (forest)
+#                                                                   |cost: 水 (water)
+#                                                                |cost: 火 (fire)
+
+# Zoom on XX columns, those are boolean for card status or 4bit integers for element and types :
 #                                                                                                |bool for limits          0000=無 0   neutral
 #                                                                                                |風                        0100=風 4   wind
 #                                                                                                  |森                      0011=森 3   forest
@@ -52,6 +53,13 @@
 # A card without ability description will show x0000
 # then follows a variable length bytes run and the next card pattern starts
 
+# also description may contain placeholders for icons
+# ex from last card (last in pointer table)
+# 83 8F 83 43 83 8B 83 68 83 4F 83 8D 81 5B 83 58 00 93 79 92 6E 82 F0 0E 04 83 0E 01 82 C9 95 CF 89 BB 82 B3 82 B9 82 E9 00
+#                                                                     |icon placehold|
+# Pattern is 0E 04 XX 0E 01, where XX in [80,84] (the 5 elements)
+
+
 #!/usr/bin/env python3
 
 # card data offsets
@@ -71,11 +79,25 @@ with open(meta.DT0, 'rb') as f:
 
 
 
-card_pattern = re.compile(b'\\x83\\x8F\\x83\\x43\\x83\\x8B\\x83\\x68\\x83\\x4F\\x83\\x8D\\x81\\x5B\\x83\\x58')
+card_pattern = re.compile(b'\x83\\x8F\\x83\\x43\\x83\\x8B\\x83\\x68\\x83\\x4F\\x83\\x8D\\x81\\x5B\\x83\\x58')
 for m in re.finditer(card_pattern, data):
     print('x%02x-x%02x: %s' % (m.start(), m.end(), m.group(0).decode('shift_jisx0213')))
 
-card_pattern = re.compile(b'\\x00{5}([\\x00-\\xFF])\\x00([\\x01-\\xFF])\\x00([\\x01-\\xFF])\\x00([\\x00-\\x03])')
+#                           header
+#                                     1:ST
+#                                                        2:HP
+#                                                                            3:G
+#                                                                                                4:R
+#                                                                                                               5:bools0 : ?
+#                                                                                                                              6:bools1 : ?
+#                                                                                                                                                  7:bools2 : limits
+#                                                                                                                                                                 8:bools3 : types+elements
+#                                                                                                                                                                                      9:cost:fire
+#                                                                                                                                                                                                     10:cost:water
+#                                                                                                                                                                                                                    11:cost:forest
+#                                                                                                                                                                                                                                   12:cost:wind
+#                                                                                                                                                                                                                                                 13:squares
+card_pattern = re.compile(b'\\x00{5}([\\x00-\\xFF])\\x00([\\x01-\\xFF])\\x00([\\x01-\\xFF])\\x00([\\x00-\\x03])([\\x00-\\xFF])([\\x00-\\xFF])\\x00([\\x00-\\xFF])([\\x00-\\xFF])\\x00([\\x00-\\xFF])([\\x00-\\xFF])([\\x00-\\xFF])([\\x00-\\xFF])([\\x00-\\xFF])\\x00{11}')
 
 ## search all cards from using offset table
 block_offsets = extract_blocks_from_offset_table(data, _TABLE_START, _TABLE_END);
@@ -85,13 +107,22 @@ print("Card blocks found from offset table: "+ str(len(block_offsets.keys())))
 
 c=0
 for m in re.finditer(card_pattern, data):
-    print('x%02x-x%02x: %s ST:%d HP:%d G:%d R:%d' % (
+    print('x%02x-x%02x: %s\nST:%d HP:%d G:%d R:%d bools0:%02x bools1:%02x bools2:%02x bools3:%02x cost(fire):%d cost(water):%d cost(forest):%d cost(wind):%d squares:%d' % (
             m.start(), m.end(),
             m.group(0), #.decode('shift_jisx0213')
-            int.from_bytes(m.group(1), byteorder='little'),
-            int.from_bytes(m.group(2), byteorder='little'),
-            int.from_bytes(m.group(3), byteorder='little'),
-            int.from_bytes(m.group(4), byteorder='little'),
+            int.from_bytes(m.group(1), byteorder='little'), #ST
+            int.from_bytes(m.group(2), byteorder='little'), #HP
+            int.from_bytes(m.group(3), byteorder='little'), #G
+            int.from_bytes(m.group(4), byteorder='little'), #R
+            int.from_bytes(m.group(5), byteorder='little'), #bools0
+            int.from_bytes(m.group(6), byteorder='little'),
+            int.from_bytes(m.group(7), byteorder='little'),
+            int.from_bytes(m.group(8), byteorder='little'),
+            int.from_bytes(m.group(9), byteorder='little'), #cost: fire
+            int.from_bytes(m.group(10), byteorder='little'), # water
+            int.from_bytes(m.group(11), byteorder='little'), # forest
+            int.from_bytes(m.group(12), byteorder='little'), # wind
+            int.from_bytes(m.group(13), byteorder='little'), # squares
         )
     )
     c+=1
